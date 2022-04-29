b07e1f926b75bb822c4f73bfdbce468147f8bf7f
Juan A <jp-27@outlook.com>
2022-04-29 13:45:02 -0400

    Finished working on all of ch1 of the book

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,10 +1,34 @@
 #! /usr/bin/env python3
+"""
+Author: Juan Arevalo <jp-27@outlook.com>
+This is work done following the book Tiny Python Projects
+    by Ken Youens-Clark <kyclark@gmail.com>
 # Purpose: Say hello
+"""
+
 
 import argparse
 
-parser = argparse.ArgumentParser(description='Say Hello')
-parser.add_argument('-n', '--name', metavar='name', default='World', help='Name to greet')
-args = parser.parse_args()
 
-print('Hello, ' + args.name + '!')
+#-------------------------------------------
+def get_args():
+    """Get the command-line arguments"""
+
+    parser = argparse.ArgumentParser(description='Say Hello')
+    parser.add_argument('-n',
+                        '--name',
+                        metavar='name',
+                        default='World',
+                        help='Name to greet')
+    return parser.parse_args()
+
+#-------------------------------------------
+def main():
+    """Runs the program"""
+    args = get_args()
+    print('Hello, ' + args.name + '!')
+
+
+#-------------------------------------------
+if __name__ == '__main__':
+    main()
